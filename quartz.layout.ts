5527dcc91b1a0404a3c6cff74015778c47aec29a
HashemAlavi <93095783+HashemAlavi@users.noreply.github.com>
2025-07-09 22:49:11 +0430

    .

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,8 +23,7 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "telegram": "https://t.me/Hashem_Alavi"
     },
   }),
 }
