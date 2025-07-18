7cd74cf1b55b3d58a28913fdb47bd056789a4612
HashemAlavi <93095783+HashemAlavi@users.noreply.github.com>
2025-07-19 02:42:25 +0430

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -63,7 +63,6 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
   }),
 ),
